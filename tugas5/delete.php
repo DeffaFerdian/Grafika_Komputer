4aeb84e3223d1662a0548678c7155067c6df9a76
DeffaFerdian <55156050+DeffaFerdian@users.noreply.github.com>
2022-06-22 15:10:46 +0700

    Delete tugas5 directory

--- tugas5/delete.php
+++
@@ -1,7 +0,0 @@
-<?php
-include 'koneksi.php';
-  if(isset($_GET['nim'])){
-    $delete = mysqli_query($conn, "DELETE FROM newtugas5 WHERE nim = '".$_GET['nim']."' ");
-    header('location:index.php');
-}
-?>
--- tugas5/koneksi.php
+++
@@ -1,6 +0,0 @@
-<?php
-$conn = mysqli_connect("localhost","root","","tugas5");
-if(!$conn){
-  echo "gagal terhubung";
-} 
-?>
